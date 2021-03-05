0554dde484edf622e67533bb287d5ac9fdde5e19
K0baU <63403741+K0baU@users.noreply.github.com>
2021-03-05 21:27:42 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let order;
+for (let i = 0; i < 39; i++) {
+    order[i]=i+1;
+}
+let randomOrder;
+for (let i = 0; i < order.length; i++) {
+    const random=Math.floor(Math.random()*(order.length-i));
+    randomOrder[i] = order[random];
+    order[i]=order[random];
+}
+document.getElementById("random").innerHTML=randomOrder;
